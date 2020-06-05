f6216d45f54e63decadd1ac23907d32f20ec17db
Alan Shan <admin@bhod.ga>
2020-06-05 13:41:42 +0300

    Final

--- app/src/main/java/top/ilum/exchanger/MainActivity.kt
+++ app/src/main/java/top/ilum/exchanger/MainActivity.kt
@@ -22,23 +22,28 @@ import java.time.format.DateTimeFormatter
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         val storage = getSharedPreferences("savedRates", MODE_PRIVATE)
+        val okHttpClient = OkHttpClient()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Convert_button.setOnClickListener {
             if (Number1.text.isNotEmpty()) {
+                var clipper: String
+                var calculatedValue: String
+                val currencyValue = Number1.text.toString()
+                val currencyFrom = Currency1.selectedItem.toString()
+                val currencyTo = Currency2.selectedItem.toString()
+                val url = "https://api.exchangeratesapi.io/latest?base=$currencyFrom"
+                val request: Request = Request.Builder().url(url).build()
                 val keyboard = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
                 if (keyboard.isAcceptingText) {
                     keyboard.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
                 }
-                val okHttpClient = OkHttpClient()
-                val url = "https://api.exchangeratesapi.io/latest?base=" + Currency1.selectedItem
-                val request: Request = Request.Builder().url(url).build()
-                var clipper: String
+
                 fun mathCheck(value: Double): String {
                     return if (value % 1.0 == 0.0) {
-                        "%.0f"
+                        "%.0f".format(value)
                     } else {
-                        "%.2f"
+                        "%.2f".format(value)
                     }
                 }
 
@@ -61,30 +66,36 @@ class MainActivity : AppCompatActivity() {
                     popUp.show()
 
                 }
+                if (currencyFrom !== currencyTo) {
                 okHttpClient.newCall(request).enqueue(object : Callback {
                     @RequiresApi(VERSION_CODES.O)
                     override fun onFailure(call: Call, e: IOException) {
                         if (storage.contains("savedRates")) {
                             val savedRates =
                                 JSONObject(storage.getString("savedRates", "def") as String)
-                            val selectedDestCurrency = savedRates.getJSONObject("rates")
-                                .get(Currency2.selectedItem.toString()).toString().toDouble()
-                            val calculatedValue: String
-                            calculatedValue =
-                                if (savedRates.get("base").toString() == Currency1.selectedItem) {
-                                    val mathThingy =
-                                        Number1.text.toString().toDouble() * selectedDestCurrency
-                                    clipper = mathCheck(mathThingy).format(mathThingy)
-                                    Number1.text.toString() + " " + Currency1.selectedItem + " = " + clipper + " " + Currency2.selectedItem
-                                } else {
-                                    val mathThingy = Number1.text.toString()
-                                        .toDouble() / savedRates.getJSONObject("rates")
-                                        .get(Currency1.selectedItem.toString()).toString()
-                                        .toDouble() * selectedDestCurrency
-                                    clipper = mathCheck(mathThingy).format(mathThingy)
-                                    Number1.text.toString() + " " + Currency1.selectedItem + " = " + clipper + " " + Currency2.selectedItem
+                            if (currencyTo !== currencyFrom) {
+                                val selectedDestCurrency = savedRates.getJSONObject("rates")
+                                    .get(currencyTo).toString().toDouble()
+                                calculatedValue =
+                                    if (savedRates.get("base").toString() == currencyFrom) {
+                                        val mathThingy =
+                                            currencyValue.toDouble() * selectedDestCurrency
+                                        clipper = mathCheck(mathThingy)
+                                        "$currencyValue $currencyFrom = $clipper $currencyTo"
+                                    } else {
+                                        val mathThingy = currencyValue
+                                            .toDouble() / savedRates.getJSONObject("rates")
+                                            .get(currencyFrom).toString()
+                                            .toDouble() * selectedDestCurrency
+                                        clipper = mathCheck(mathThingy).format(mathThingy)
+                                        "$currencyValue $currencyFrom = $clipper $currencyTo"
 
-                                }
+                                    }
+                            } else {
+                                calculatedValue =
+                                    "$currencyValue $currencyFrom = $currencyValue $currencyTo"
+                                clipper = currencyValue
+                            }
                             val date = LocalDate.parse(savedRates.get("date") as CharSequence?)
                                 .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
                             showResult(
@@ -101,19 +112,30 @@ class MainActivity : AppCompatActivity() {
                     }
 
                     override fun onResponse(call: Call?, response: Response?) {
-                        val json = response?.body()?.string()
-                        storage.edit()
-                            .putString("savedRates", json).apply()
-                        val txt = JSONObject(json as String).getJSONObject("rates")
-                            .get(Currency2.selectedItem.toString()).toString()
-                        val mathThingy = Number1.text.toString().toDouble() * txt.toDouble()
-                        clipper = mathCheck(mathThingy).format(mathThingy)
-                        val calculatedValue =
-                            Number1.text.toString() + " " + Currency1.selectedItem + " = " + clipper + " " + Currency2.selectedItem
-                        showResult(calculatedValue, clipper)
+                        if (currencyFrom !== currencyTo) {
+                            val json = response?.body()?.string()
+                            storage.edit()
+                                .putString("savedRates", json).apply()
+                            val txt = JSONObject(json as String).getJSONObject("rates")
+                                .get(currencyTo).toString()
+                            val mathThingy = currencyValue.toDouble() * txt.toDouble()
+                            clipper = mathCheck(mathThingy)
+                            calculatedValue =
+                                "$currencyValue $currencyFrom = $clipper $currencyTo"
+                        } else {
+                            clipper = currencyValue
+                            calculatedValue = "$currencyValue $currencyFrom = $clipper $currencyTo"
+                            showResult(calculatedValue, clipper)
+                        }
                     }
-                })
-            } else {
+                }) }
+                else {
+                    clipper = currencyValue
+                    calculatedValue = "$currencyValue $currencyFrom = $clipper $currencyTo"
+                    showResult(calculatedValue, clipper)
+                }
+            }
+            else {
                 Number1.error = "Поле не может быть пустым!"
             }
         }
